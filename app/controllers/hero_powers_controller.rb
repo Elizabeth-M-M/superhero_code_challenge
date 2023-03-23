e269c3e34f8f7fb8095027264d212e2f34e76a2b
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-23 20:32:15 +0300

    Defined the create route in HeroPowers controller

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,15 @@
 class HeroPowersController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_error
+
+  def create
+    
+    h_power= HeroPower.create!(params.permit(:strength, :hero_id, :power_id))
+    render json: h_power
+  end
+
+  private
+  
+  def render_unprocessable_error(invalid)
+    render json: {errors:invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
 end
