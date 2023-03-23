c09075bbb21e0e2f2d65f40f3447fe65dbd5de0c
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-23 20:21:01 +0300

    Defined the index and show methods in the Heros controller

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,17 @@
 class HerosController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def index
+    render json: Hero.all
+  end
+  def show
+    hero= Hero.find(params[:id])
+    render json: hero
+  end
+
+  private
+
+  def record_not_found
+    render json: {error:"Hero was not found"}, status: :not_found
+  end
 end
