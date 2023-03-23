33620854778523f980f2b4235502000c01bbb928
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-23 20:28:42 +0300

    Defined the methods in the Powers controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,29 @@
 class PowersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_error
+
+  def index
+    render json: Power.all
+  end
+  def show
+    power= find_power
+    render json: power
+  end
+  def update
+    power= find_power
+    power.update!(params.permit(:name, :description))
+    render json: power
+  end
+
+  private
+  def find_power
+    Power.find(params[:id])
+  end
+
+  def record_not_found
+    render json: {error:"Power was not found"}, status: :not_found
+  end
+  def render_unprocessable_error(invalid)
+    render json: {errors:invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
 end
