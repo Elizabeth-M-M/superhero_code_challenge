45ef4d2f237c6ce1b65f9b8318ea3231dd846678
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-23 20:04:51 +0300

    Created some seed data for the application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,77 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Starts seeding....."
+heroes= Hero.create!(
+  [
+    {
+      name:"Spider-Man",
+      super_name:"Spider-Man"
+    },
+    {
+      name:"Hulk",
+      super_name:"The incredible hulk"
+    },
+    {
+      name:"Jean Grey",
+      super_name:"Jean Grey"
+    }
+  ]
+)
+powers= Power.create!(
+  [
+    {
+      name:"Spider-sense.",
+      description:"It's a limited precognition, allowing him to anticipate danger and avoid it."
+    },
+    {
+      name:"Healing Factor",
+      description:"Ability to recover from bodily injuries or disease at a superhuman rate"
+    },
+    {
+      name:"Superhuman Strength",
+      description:"Extremely great strength"
+    },
+    {
+      name:"Gamma bomb",
+      description:"One gets stronger when he gets angry "
+    },
+    {
+      name:"Telepath",
+      description:"Supposed communication of thoughts or ideas by means other than the known senses"
+    }
+
+  ]
+)
+hero_powers= HeroPower.create!(
+  [
+    {
+      strength:"Strong",
+      hero_id:1,
+      power_id:1
+    },
+    {
+      strength:"Average",
+      hero_id:3,
+      power_id:5
+    },
+    {
+      strength:"Weak",
+      hero_id:2,
+      power_id:4
+    },
+    {
+      strength:"Strong",
+      hero_id:2,
+      power_id:3
+    },
+    {
+      strength:"Strong",
+      hero_id:1,
+      power_id:2
+    },
+    {
+      strength:"Weak",
+      hero_id:1,
+      power_id:3
+    }
+  ]
+)
+puts "done"
