d9e1ac27a6ab8c1172faef8ab8d8eb8026addfb0
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-24 12:30:55 +0300

    Added a custom serializer to the show method

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -6,12 +6,12 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   end
   def show
     hero= Hero.find(params[:id])
-    render json: hero
+    render json: hero, serializer:HeroWithPowersSerializer 
   end
 
   private
 
   def record_not_found
-    render json: {error:"Hero was not found"}, status: :not_found
+    render json: {error:"Hero not found"}, status: :not_found
   end
 end
